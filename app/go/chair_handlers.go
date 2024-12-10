4d43092c0bf8223da570ac534e19583efdb970f7
mazrean <shunsuke.w04@gmail.com>
2024-12-11 07:22:01 +0900

    Revert "chair status cache"
    This reverts commit e69482f4c1c63edad74d5fd2e52a9175771d63f1.

--- app/go/chair_handlers.go
+++ app/go/chair_handlers.go
@@ -206,18 +206,6 @@ func chairPostCoordinate(w http.ResponseWriter, r *http.Request) {
 
 	if newStatus != nil {
 		rideStatusesCache.Store(ride.ID, newStatus)
-		var badgerStatus rideStatus
-		switch newStatus.Status {
-		case "PICKUP":
-			badgerStatus = rideStatusPickUp
-		case "ARRIVED":
-			badgerStatus = rideStatusArrived
-		}
-		if err := updateChairStatusToBadger(chair.ID, badgerStatus); err != nil {
-			writeError(w, r, http.StatusInternalServerError, err)
-			return
-		}
-
 		ChairPublish(chair.ID, &RideEvent{
 			status: newStatus.Status,
 			rideID: ride.ID,
@@ -489,18 +477,6 @@ func chairPostRideStatus(w http.ResponseWriter, r *http.Request) {
 		ID:     statusID,
 		Status: req.Status,
 	})
-
-	var badgerStatus rideStatus
-	switch req.Status {
-	case "ENROUTE":
-		badgerStatus = rideStatusEnRoute
-	case "CARRYING":
-		badgerStatus = rideStatusCarrying
-	}
-	if err := updateChairStatusToBadger(chair.ID, badgerStatus); err != nil {
-		writeError(w, r, http.StatusInternalServerError, err)
-		return
-	}
 
 	ChairPublish(chair.ID, &RideEvent{
 		status: req.Status,
