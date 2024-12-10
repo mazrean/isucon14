717c0756bc9a29c6f3071077115d6df7de67ee3c
mazrean <shunsuke.w04@gmail.com>
2024-12-10 12:46:28 +0900

    Revert "app notification sse fix"
    This reverts commit d712aa51b2e9eec90a21ebd608aad159351e3719.

--- app/go/app_handlers.go
+++ app/go/app_handlers.go
@@ -782,7 +782,6 @@ func appGetNotification(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
-	w.Header().Set("X-Accel-Buffering", "no")
 
 	sb := &strings.Builder{}
 	err = json.NewEncoder(sb).Encode(response)
