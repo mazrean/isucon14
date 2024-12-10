18d8e81211794d3623d6864ba67fd75a6a565948
mazrean <shunsuke.w04@gmail.com>
2024-12-10 12:45:43 +0900

    Revert "app notification sse fix"
    This reverts commit 83df9e712553d445e005bb476c1b1f6cb590a259.

--- app/go/chair_handlers.go
+++ app/go/chair_handlers.go
@@ -184,7 +184,6 @@ func chairPostCoordinate(w http.ResponseWriter, r *http.Request) {
 	})
 
 	rideStatusesCache.Forget(ride.ID)
-	notificationResponseCache.Forget(ride.UserID)
 	if newStatus != "" {
 		Publish(ride.ID, &RideEvent{
 			status: newStatus,
@@ -410,7 +409,6 @@ func chairPostRideStatus(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	rideStatusesCache.Forget(ride.ID)
-	notificationResponseCache.Forget(ride.UserID)
 
 	Publish(ride.ID, &RideEvent{
 		status: req.Status,
