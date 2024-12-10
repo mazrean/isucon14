6824fe40f9932c29a7db0b5c166e76d899c33c80
mazrean <shunsuke.w04@gmail.com>
2024-12-10 12:45:50 +0900

    Revert "near by debug"
    This reverts commit b379cba7a776ea8b9edd623d0c3883f46058372d.

--- app/go/event_bus.go
+++ app/go/event_bus.go
@@ -3,9 +3,6 @@ package main
 import (
 	"sync"
 	"time"
-
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 type RideEvent struct {
@@ -38,28 +35,7 @@ func Publish(event string, message *RideEvent) {
 	eventBusLock.RLock()
 	defer eventBusLock.RUnlock()
 
-	statusGauge.WithLabelValues(message.status).Inc()
-	switch message.status {
-	case "MATCHED":
-		statusGauge.WithLabelValues("MATCHING").Dec()
-	case "ENROUTE":
-		statusGauge.WithLabelValues("MATCHED").Dec()
-	case "PICKUP":
-		statusGauge.WithLabelValues("ENROUTE").Dec()
-	case "CARRYING":
-		statusGauge.WithLabelValues("PICKUP").Dec()
-	case "ARRIVED":
-		statusGauge.WithLabelValues("CARRYING").Dec()
-	case "COMPLETED":
-		statusGauge.WithLabelValues("ARRIVED").Dec()
-	}
-
 	for _, ch := range eventBus[event] {
 		ch <- message
 	}
 }
-
-var statusGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-	Name: "chair_status",
-	Help: "chair status",
-}, []string{"status"})
