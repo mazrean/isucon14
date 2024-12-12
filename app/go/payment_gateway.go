19c691669bef66223bb0f995e19ff39b49c905e7
mazrean <shunsuke.w04@gmail.com>
2024-12-12 10:36:20 +0900

    requestPaymentGatewayPostPayment

--- app/go/payment_gateway.go
+++ app/go/payment_gateway.go
@@ -5,10 +5,11 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"log/slog"
 	"net/http"
-	"time"
 
 	"github.com/goccy/go-json"
+	"github.com/oklog/ulid/v2"
 )
 
 var erroredUpstream = errors.New("errored upstream")
@@ -28,6 +29,8 @@ func requestPaymentGatewayPostPayment(ctx context.Context, paymentGatewayURL str
 		return err
 	}
 
+	idempotencyKey := ulid.Make().String()
+
 	// 失敗したらとりあえずリトライ
 	// FIXME: 社内決済マイクロサービスのインフラに異常が発生していて、同時にたくさんリクエストすると変なことになる可能性あり
 	retry := 0
@@ -35,59 +38,31 @@ func requestPaymentGatewayPostPayment(ctx context.Context, paymentGatewayURL str
 		err := func() error {
 			req, err := http.NewRequestWithContext(ctx, http.MethodPost, paymentGatewayURL+"/payments", bytes.NewBuffer(b))
 			if err != nil {
-				return err
+				return fmt.Errorf("failed to create request: %w", err)
 			}
 			req.Header.Set("Content-Type", "application/json")
 			req.Header.Set("Authorization", "Bearer "+token)
+			req.Header.Set("Idempotency-Key", idempotencyKey)
 
 			res, err := http.DefaultClient.Do(req)
 			if err != nil {
-				return err
+				return fmt.Errorf("failed to request payment gateway: %w", err)
 			}
 			defer res.Body.Close()
 
 			if res.StatusCode != http.StatusNoContent {
-				// エラーが返ってきても成功している場合があるので、社内決済マイクロサービスに問い合わせ
-				getReq, err := http.NewRequestWithContext(ctx, http.MethodGet, paymentGatewayURL+"/payments", bytes.NewBuffer([]byte{}))
-				if err != nil {
-					return err
-				}
-				getReq.Header.Set("Authorization", "Bearer "+token)
-
-				getRes, err := http.DefaultClient.Do(getReq)
-				if err != nil {
-					return err
-				}
-				defer res.Body.Close()
-
-				// GET /payments は障害と関係なく200が返るので、200以外は回復不能なエラーとする
-				if getRes.StatusCode != http.StatusOK {
-					return fmt.Errorf("[GET /payments] unexpected status code (%d)", getRes.StatusCode)
-				}
-				var payments []paymentGatewayGetPaymentsResponseOne
-				if err := json.NewDecoder(getRes.Body).Decode(&payments); err != nil {
-					return err
-				}
-
-				rides, err := retrieveRidesOrderByCreatedAtAsc()
-				if err != nil {
-					return err
-				}
-
-				if len(rides) != len(payments) {
-					return fmt.Errorf("unexpected number of payments: %d != %d. %w", len(rides), len(payments), erroredUpstream)
-				}
-
-				return nil
+				return fmt.Errorf("unexpected status code: %d", res.StatusCode)
 			}
 			return nil
 		}()
 		if err != nil {
 			if retry < 5 {
 				retry++
-				time.Sleep(100 * time.Millisecond)
 				continue
 			} else {
+				slog.Error("failed to request payment gateway",
+					slog.String("error", err.Error()),
+				)
 				return err
 			}
 		}
