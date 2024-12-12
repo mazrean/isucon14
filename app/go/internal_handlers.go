cf16a531df242d538a3252ab317a6545b3f6230c
mazrean <shunsuke.w04@gmail.com>
2024-12-12 10:47:30 +0900

    fix matching

--- app/go/internal_handlers.go
+++ app/go/internal_handlers.go
@@ -222,10 +222,10 @@ func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 			pd := float64(manhattanDistance(ride.PickupLatitude, ride.PickupLongitude, location.LastLatitude, location.LastLongitude)) / float64(chairModelSpeedCache[ch.Model])
 			//dd := float64(manhattanDistance(ride.PickupLatitude, ride.PickupLongitude, ride.DestinationLatitude, ride.DestinationLongitude))
 			age := int(time.Since(ride.CreatedAt).Milliseconds())
-			loss := math.Pow(float64(age)/10000, 3)
+			loss := math.Pow(float64(age)/10000, 2)
 			// 25s以上経過しているrideは優先度を大きく上げる
 			if age > 25000 {
-				loss += 10000
+				loss += 100000
 			}
 
 			// ベンチマーカーハック: ベンチマーク中にマッチングの期限を迎えないrideは割り当て優先度を下げ、終了後にマッチングさせる
