e8e4df0c6ae26751e483ac68c90538e2e3bee6f6
mazrean <shunsuke.w04@gmail.com>
2024-12-17 13:31:41 +0900

    Revert "remove nginx fix"
    This reverts commit 9f766dd71e50f4b495b0f062381351ba03619fbb.

--- app/go/main.go
+++ app/go/main.go
@@ -158,10 +158,6 @@ func setup() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if strings.HasPrefix(r.URL.Path, "/api") {
 			mux.ServeHTTP(w, r)
-		} else if r.URL.Path == "/index.html" {
-			w.Header().Set("Content-Type", "text/html")
-			w.WriteHeader(http.StatusOK)
-			http.ServeFileFS(w, r, sub, "index.html")
 		} else {
 			rw := &responseWriterInterceptor{ResponseWriter: w, statusCode: http.StatusOK}
 			publicHandler.ServeHTTP(rw, r)
